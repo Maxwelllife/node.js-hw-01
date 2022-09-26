bd5b87ee4e073b5a3e22c81e49d5814cc29c71be
Maksym Pokulytyi <mlightpro@gmail.com>
2022-09-26 18:20:14 +0300

    add commander

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const contactsOperations = require("./contacts");
-const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -48,7 +61,7 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-const arr = hideBin(process.argv);
-const { argv } = yargs(arr);
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
 
 invokeAction(argv);
